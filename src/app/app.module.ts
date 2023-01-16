25b39140ec882b0565fce4059fafce7043369207
fc0S4b <fcoj.sabugo@gmail.com>
2023-01-16 01:36:13 -0300

    set up the cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
-// esta línea se agrega automáticamente cuando creas el componente usando la línea de comnados, al igual que la línea de ProductAlertsComponent en las declaraciones de más abajo
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -18,6 +18,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      {path: 'cart', component: CartComponent}
     ])
   ],
   declarations: [
@@ -25,7 +26,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   bootstrap: [
     AppComponent
